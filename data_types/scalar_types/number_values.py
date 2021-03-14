c1e46fb5da0be507f56f9cc44e02c1f2f6fdf94c
mario <mario.briseno7@gmail.com>
2021-03-13 18:12:32 -0600

    added example of conservation of trailing zeros with decimal

--- data_types/scalar_types/number_values.py
+++ data_types/scalar_types/number_values.py
@@ -44,6 +44,15 @@ from decimal import (
 #decimal.getcontext().traps[decimal.FloatOperation] = True
 #invalid_decimal_value = Decimal(0.22)
 
+# decimal, unlike float, preserves the precision of numbers supplied
+# with trailing 0s.
+
+one_zero = Decimal("37.0")
+two_zeros = Decimal("0.00800")
+print(one_zero, "One traling zeros preserved")
+print(two_zeros, "Two trailing zeros preserved")
+
+
 a = 1.1 + 2.2
 print(a) # -> 3.3000000000000003
 print(Decimal("1.1") + Decimal("2.2")) # -> 3.3
